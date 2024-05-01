4642bbfa7b9279c9290fc0ffd25b49d2b5b1e987
Satyendra Yadav <92631021+yushprince@users.noreply.github.com>
2024-05-01 13:47:15 +0530

    Controllers

---
+++ Backend/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using _Backend.Data;
+using _Backend.DataModel;
+using _Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace _Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post(GameResult gameResult)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "game_results", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    string message = $"{gameResult.WinnerName} won {gameResult.Name}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "game_results", basicProperties: null, body: body);
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Backend/Controllers/MailingListController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using _Backend.Data;
+using _Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace _Backend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class MailingListController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MailingListController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IList<MailingList> Get()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            IList<MailingList> mailingLists = _dbContext.MailingLists.Include(x => x.MailingLists).ThenInclude(y => y.Contact).Where(y => y.UserId == userId).ToList();
+
+            //In real code I would create data models to send to front end. In hacky code I will just null out the cyclic reference
+            foreach (MailingList mailingList in mailingLists)
+            {
+                foreach (MailingListContactLink mailingListContactLink in mailingList.MailingLists)
+                {
+                    mailingListContactLink.MailingList = null;
+                    mailingListContactLink.Contact.MailingLists = null;
+                }
+            }
+
+            return mailingLists;
+        }
+    }
+}
